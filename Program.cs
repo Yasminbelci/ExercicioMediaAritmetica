4e3c4f706ddea42f3c0b7fd666b2ed05b013827d
yasmin <yasmin.belci13@gmail.com>
2022-03-23 18:23:38 -0300

    yasmin belci

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double nr1, nr2, nr3, media;
+const double constanteDivisor = 3;
+
+Console.WriteLine("Calculador de média aritmética\n");
+
+Console.Write("Insira o primeiro número: ");
+nr1 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Insira o segundo número: ");
+nr2 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Insira o terceiro número: ");
+nr3 = Convert.ToDouble(Console.ReadLine()!);
+
+media = (nr1 + nr2 + nr3) / constanteDivisor;
+
+Console.WriteLine($"A média é {media}");
